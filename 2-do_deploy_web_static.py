58dd00fed61fec70d83610638bd93a629e07c33d
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-25 00:49:29 +0100

    bruh

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,36 +2,40 @@
 """distributes an archive to your web servers"""
 
 import os.path
-import fabric.api
-fabric.api.env.hosts = ['54.82.98.129', '52.207.196.248']
+from fabric.api import run, put, env
+env.hosts = ['54.82.98.129', '52.207.196.248']
 
 
 def do_deploy(archive_path):
     """Prototype: def do_deploy(archive_path)"""
-    if os.path.exists(archive_path):
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
         file = archive_path.split("/")[-1]
         rm = file.split(".")[0]
         path = "/data/web_static/releases/"
         path2 = "/data/web_static/current"
 
-        fabric.api.put(archive_path, '/tmp/')
+        put(archive_path, '/tmp/')
 
-        fabric.api.run('mkdir -p {}{}/'.format(path, rm))
+        run('mkdir -p {}{}/'.format(path, rm))
 
-        fabric.api.run('tar -zxvf /tmp/{} -C {}{}/'.format(file, path, rm))
+        run('tar -zxf /tmp/{} -C {}{}/'.format(file, path, rm))
 
-        fabric.api.run('rm /tmp/{}'.format(file))
+        run('rm /tmp/{}'.format(file))
 
-        fabric.api.run('mv {0}{1}/web_static/* {0}{1}/'.format(path, rm))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, rm))
 
-        fabric.api.run('rm -rf {}{}/web_static'.format(path, rm))
+        run('rm -rf {}{}/web_static'.format(path, rm))
 
-        fabric.api.run('rm -rf {}'.format(path2))
+        run('rm -rf {}'.format(path2))
 
-        fabric.api.run('ln -s {}{}/ {}'.format(path, rm, path2))
+        run('ln -s {}{}/ {}'.format(path, rm, path2))
 
         print("New version deployed!")
 
         return True
 
-    return False
+    except ValueError:
+
+        return False
