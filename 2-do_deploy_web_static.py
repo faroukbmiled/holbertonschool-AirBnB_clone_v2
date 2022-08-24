d88d431a0f5d28ad36ad1cadd96880c31290394b
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-25 00:20:39 +0100

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,24 +17,14 @@ def do_deploy(archive_path):
         path2 = "/data/web_static/current"
 
         fabric.api.put(archive_path, '/tmp/')
-
         fabric.api.run('mkdir -p {}{}/'.format(path, rm))
-
         fabric.api.run('tar -zxf /tmp/{} -C {}{}/'.format(file, path, rm))
-
         fabric.api.run('rm -f /tmp/{}'.format(file))
-
         fabric.api.run('mv -f {0}{1}/web_static/* {0}{1}/'.format(path, rm))
-
         fabric.api.run('rm -rf {}{}/web_static'.format(path, rm))
-
         fabric.api.run('rm -rf {}'.format(path2))
-
         fabric.api.run('ln -fs {}{}/ {}'.format(path, rm, path2))
-
-        print ("New version deployed!")
-        
+        print("New version deployed!")
         return True
-
     except ValueError:
         return False
