f91b24e2613b026a288fc0d8f33e810d42e1020a
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-25 00:07:34 +0100

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,7 @@
 sudo apt-get -y update && upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-cfg=\
-"<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"
-echo "$cfg" | sudo tee /data/web_static/releases/test/index.html
+echo $'<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
