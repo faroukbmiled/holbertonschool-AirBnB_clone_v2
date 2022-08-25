86f1cd77851adae7e0f1abdb1d01c1eb6618f7cd
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-25 12:38:09 +0100

    final

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo $'<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '37 a location /hbnb_static/ {\ntalias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
+sudo sed -i '26 a location /hbnb_static/ {\ntalias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
 sudo service nginx stop
 sudo service nginx start
