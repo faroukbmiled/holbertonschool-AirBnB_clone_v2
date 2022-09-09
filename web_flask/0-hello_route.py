fd5fab70e1c297246c938bd7855b3ab7cfcc34e6
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-09 13:21:44 +0100

    initial

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""task 0"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def flask_hello():
+    """prints the bellow return msg!"""
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
